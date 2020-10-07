c70c718d2a19b5536432ac71bb124441ee1d34e5
John Akhilomen <jonbasanti@gmail.com>
2020-10-06 19:05:44 -0500

    Worked on router generator

--- dist/bin/generateMongooseModel.js
+++ dist/bin/generateMongooseModel.js
@@ -1,4 +1,6 @@
 #! /usr/bin/env node
+const {ControllerGenerator} = require("../src/impls/ControllerGenerator");
+const {RouterGenerator} = require("../src/impls/RouterGenerator");
 
 (async function () {
     try
@@ -28,10 +30,7 @@
         const innerArr = att.split(":");
         modelObj[`${innerArr[0]}`] = innerArr[1];
     });
-    
-    //console.log(modelObj);
-    
-    
+ 
     let filecontent = `const mongoose = require("mongoose");
 const ${modelName}Schema = mongoose.Schema({\n`;
     
@@ -114,97 +113,12 @@ const ${modelName}Schema = mongoose.Schema({\n`;
       
     const modelNameWithoutModel = modelName.charAt(0).toUpperCase() + modelName.replace("Model", "").slice(1) ;
    
-    //Make content for controller 
-    const controllerFileContent = `
-const ${modelName} = require('../../src/models/${modelName}');
+let controllerGenerator = new ControllerGenerator(`${currentDir}/src/controllers/${modelNameWithoutModel}Controller.js`, modelName, modelNameWithoutModel);
+controllerGenerator.writeToController();
+   
+let routerGenerator = new RouterGenerator(`${currentDir}/src/routers/${modelNameWithoutModel}Router.js`, modelName, modelNameWithoutModel);
+routerGenerator.writeToRouter();
 
-// Display list of all ${modelNameWithoutModel}.
-exports.${modelNameWithoutModel}_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: ${modelNameWithoutModel} list');
-};
-
-// Display detail page for a specific ${modelNameWithoutModel}.
-exports.${modelNameWithoutModel}_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: ${modelNameWithoutModel} detail: ' + req.params.id);
-};
-
-// Display ${modelNameWithoutModel} create form on GET.
-exports.${modelNameWithoutModel}_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: ${modelNameWithoutModel} create GET');
-};
-
-// Handle ${modelNameWithoutModel} create on POST.
-exports.${modelNameWithoutModel}_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: ${modelNameWithoutModel} create POST');
-};
-
-// Display ${modelNameWithoutModel} delete form on GET.
-exports.${modelNameWithoutModel}_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: ${modelNameWithoutModel} delete GET');
-};
-
-// Handle ${modelNameWithoutModel} delete on POST.
-exports.${modelNameWithoutModel}_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: ${modelNameWithoutModel} delete POST');
-};
-
-// Display ${modelNameWithoutModel} update form on GET.
-exports.${modelNameWithoutModel}_update_get = function(req, res) {
-    user.send('NOT IMPLEMENTED: ${modelNameWithoutModel} update GET');
-};
-
-// Handle ${modelNameWithoutModel} update on POST.
-exports.${modelNameWithoutModel}_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: ${modelNameWithoutModel} update POST');
-};
-
-`;
-//write content on controller file
-    const srcControllerDirname = path.dirname(`${currentDir}/src/controllers`);
-        if (!fs.existsSync(srcControllerDirname)) {
-            throw new Error ("controllers folder doesn't exist.");
-        }
-    await fs.promises.writeFile(`${currentDir}/src/controllers/${modelNameWithoutModel}Controller.js`, `${controllerFileContent}`, 'utf8');
-
-
-    //router file content
-    const routerFileContent = `
-const express = require('express');
-const router = express.Router();
-
-// Require controller modules.
-const ${modelNameWithoutModel}_controller = require('../../src/controllers/${modelNameWithoutModel}Controller');
-
-/// ${modelNameWithoutModel} ROUTES ///
-
-// GET request for one ${modelNameWithoutModel}.
-router.get('/:id', ${modelNameWithoutModel}_controller.${modelNameWithoutModel}_detail);
-
-// GET request for list of all ${modelNameWithoutModel} items.
-router.get('/${modelNameWithoutModel}', ${modelNameWithoutModel}_controller.${modelNameWithoutModel}_list);
-
-// GET request for creating a ${modelNameWithoutModel}. NOTE ${modelNameWithoutModel} (uses id).
-router.get('/create', ${modelNameWithoutModel}_controller.${modelNameWithoutModel}_create_get);
-
-// GET request to delete ${modelNameWithoutModel}.
-router.get('/:id/delete', ${modelNameWithoutModel}_controller.${modelNameWithoutModel}_delete_get);
-
-// POST request to delete ${modelNameWithoutModel}.
-router.post('/:id/delete', ${modelNameWithoutModel}_controller.${modelNameWithoutModel}_delete_post);
-
-// GET request to update ${modelNameWithoutModel}.
-router.get('/:id/update', ${modelNameWithoutModel}_controller.${modelNameWithoutModel}_update_get);
-
-// POST request to update ${modelNameWithoutModel}.
-router.post('/:id/update', ${modelNameWithoutModel}_controller.${modelNameWithoutModel}_update_post);
-
-module.exports = router;
-
-    `;
-
-await fs.promises.writeFile(`${currentDir}/src/routers/${modelNameWithoutModel}Router.js`, `${routerFileContent}`, 'utf8');
-
-  
 var data = fs.readFileSync(`${currentDir}/src/server.js`).toString().split("\n");
 data.splice((data.length-2), 0, `
 const ${modelNameWithoutModel}_router = require('./routers/${modelNameWithoutModel}Router');
